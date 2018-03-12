362189496223ee3d2566f36c4647b209fce4f13e
techbin <satish.prg@gmail.com>
2018-03-12 23:39:00 +1030

    blender to threejs commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>My first three.js app</title>
+		<title>Blender 3D model -load in three.js app</title>
 		<style>
 			body { margin: 0; }
 			canvas { width: 100%; height: 100% }
@@ -16,19 +16,12 @@
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
 
-
-
-//			var geometry = new THREE.BoxGeometry( 2, 2, 2 );
-//			var material = new THREE.MeshNormalMaterial();//MeshBasicMaterial( { color: 0x00fff0 } );
-//			var cube = new THREE.Mesh( geometry, material );
-//			scene.add( cube );
-
 			camera.position.z = 5;
 
 			var animate = function () {
 				requestAnimationFrame( animate );
 
-//				cube.rotation.x += 0.1;
+				//cube.rotation.x += 0.1;
 				//cube.rotation.y += 0.1;
 	
 			renderer.render(scene, camera);
@@ -39,7 +32,7 @@
 
 
 
-function rotate() {
+			function rotate() {
 				var timer = Date.now() * 0.0005;
 				camera.position.x = Math.cos( timer ) * 8;
 				camera.position.y = 1;
@@ -49,52 +42,31 @@ function rotate() {
 			}
 
 
-animate();
+			animate();
+
+			// instantiate a loader
+			var loader = new THREE.JSONLoader();
+			loader.load('models/newbox.json', function(geometry) {
+				material = new THREE.MeshBasicMaterial( {  side: THREE.BackSide , wireframe: false } );//color: 0x00ff00,
+			  
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// instantiate a loader
-var loader = new THREE.JSONLoader();
-loader.load('models/newbox.json', function(geometry) {
-	material = new THREE.MeshBasicMaterial( {  side: THREE.BackSide , wireframe: false } );//color: 0x00ff00,
-  
-
-
-    mesh = new THREE.Mesh(geometry,material);
-    //mesh.material.color.setHex( 0xff0000 );
-    mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.75;
-    mesh.translation = geometry.center(geometry);
-//here is the funcion defined and attached to the  object
-mesh.setColor = function(color){
-     mesh.material.color = new THREE.Color(color);
-}
-//mesh.setColor(0xFFFFFF);  //change color using hex value or
-mesh.setColor("blue") ;   //set material color by using color name
-scene.add(mesh);
-//edges                             =   new THREE.EdgesHelper(mesh, 0xff0000);
-//edges.material.linewidth          =   1;
-//scene.add(edges);
-});
+		    mesh = new THREE.Mesh(geometry,material);
+		    //mesh.material.color.setHex( 0xff0000 );
+		    mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.75;
+		    mesh.translation = geometry.center(geometry);
+			//here is the funcion defined and attached to the  object
+			mesh.setColor = function(color){
+		    mesh.material.color = new THREE.Color(color);
+			}
+			
+			//mesh.setColor(0xFFFFFF);  //change color using hex value or
+			mesh.setColor("blue") ;   //set material color by using color name
+			scene.add(mesh);
+			//edges                             =   new THREE.EdgesHelper(mesh, 0xff0000);
+			//edges.material.linewidth          =   1;
+			//scene.add(edges);
+			});
 
 
 		</script>
